9aec6d0343bf9f46c6be3d711799bffe8c2a4309
Matheus Lira <matheustlira@gmail.com>
2021-06-29 19:05:37 -0300

    First register layout without sending request to back

---
+++ src/Structures/RegLoginCont.js
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+const RegLoginCont = styled.div`
+  background-color: #316a37;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 30px;
+  a {
+    text-decoration: none;
+  }
+  form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+  }
+  input {
+    width: 100%;
+    height: 48px;
+    margin-bottom: 13px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    font-size: 20px;
+    ::placeholder {
+      font-size: 20px;
+      padding: 10px;
+    }
+  }
+  button {
+    height: 46px;
+    width: 80%;
+    background-color: #5ea064;
+    border: none;
+    color: #fff;
+    font-size: 23px;
+    font-weight: bold;
+    border-radius: 5px;
+    margin-top: 20px;
+  }
+  .title {
+    color: #fff;
+    font-family: "Ruda";
+    font-size: 32px;
+    margin-bottom: 25px;
+  }
+  .register {
+    color: #fff;
+    margin-top: 10px;
+    font-weight: bold;
+  }
+  img {
+    width: 80px;
+  }
+`;
+
+export { RegLoginCont };
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,80 @@
-export default function SignUp(){
-    return (
-        <></>
-    );
+import { RegLoginCont } from "../Structures/RegLoginCont";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+import Logo from "../images/Logo.png";
+
+export default function SignUp() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
+  const [disabled, setDisabled] = useState(false);
+
+  const history = useHistory();
+
+  function Subscribe(e) {
+    e.preventDefault();
+  }
+  return (
+    <>
+      <RegLoginCont>
+        <img src={Logo} alt="Logo" />
+        <p className="title">Kapyvara</p>
+        <form onSubmit={(e) => Subscribe(e)}>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="E-mail"
+            disabled={disabled}
+          ></input>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="Nome"
+            disabled={disabled}
+          ></input>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            placeholder="CPF"
+            disabled={disabled}
+          ></input>
+          <input
+            onChange={(e) => setConfirmPass(e.target.value)}
+            type="text"
+            placeholder="Telefone"
+            disabled={disabled}
+          ></input>
+          <input
+            onChange={(e) => setConfirmPass(e.target.value)}
+            type="password"
+            placeholder="Senha"
+            disabled={disabled}
+          ></input>
+          <input
+            onChange={(e) => setConfirmPass(e.target.value)}
+            type="password"
+            placeholder="Confirme a senha"
+            disabled={disabled}
+          ></input>
+
+          <button onClick={Subscribe} disabled={disabled}>
+            {disabled ? (
+              <Loader type="ThreeDots" color="#FFF" height={40} width={45} />
+            ) : (
+              "Cadastrar"
+            )}
+          </button>
+        </form>
+        <Link to="/">
+          <p className="register">Já tem uma conta? Entre agora!</p>
+        </Link>
+      </RegLoginCont>
+    </>
+  );
 }
