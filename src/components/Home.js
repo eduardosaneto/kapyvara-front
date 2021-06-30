7de5524682c366b5cf6be29e5e7b07f1009a31ee
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-06-30 18:00:12 -0300

    layout de apresentação dos produtos

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,14 +12,16 @@ export default function Home(){
             <Spotlight>
                 
             </Spotlight>
-            {/* <Products /> */}
+            <Products />
         </Container>
     );
 }
 
 const Container = styled.section`
+    max-width: 600px;
     width: 100%;
     height: 100%;
+    margin: 0 auto;
     padding: 115px 10px 70px 10px;
     display: flex;
     flex-direction: column;
@@ -29,8 +31,8 @@ const Container = styled.section`
 `;
 
 const Spotlight = styled.div`
-    height: 150px;
+    height: 210px;
     width: 100%;
-    background-color: #316a37;
+    background-color: #4C7E47;
 `;
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,83 @@
 import styled from "styled-components";
 import axios from "axios";
+import carteira from "../images/carteira.JPG";
 
 export default function Products(){
     return (
-        <ul>
+        <List>
             <li>
-
+                <div>
+                    <img src={carteira} alt="carteira"/>
+                </div>
+                <ProductInfos>
+                    <span>
+                        <h1>Kapyvara Leather Slim</h1>
+                    </span>
+                    <h2>R$200,00</h2>
+                </ProductInfos>                
             </li>
-            <li>
-
-            </li>
-        </ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </List>
     );
 }
+
+const List = styled.ul`
+    width: 100%;
+    display: flex;
+    padding: 20px 0px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    li {
+        width: calc(50% - 5px);
+        height: 210px;
+        margin-bottom: 8px; 
+        border: 1px solid #4C7E47;       
+        border-radius: 5px;
+    }
+    div:first-child {
+        height: 140px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-bottom: 0.5px solid #4C7E47;
+    }
+    img {
+        width: 100%;
+        max-width: 162px;
+        max-height: 118px;
+        object-fit: cover;
+    }
+`;
+
+const ProductInfos = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    padding: 2px 5px;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    span {
+        height: 30px;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 5px;
+    }
+    h1 {
+        font-size: 14px;
+        text-align: center;
+        line-height: 15px;
+    }
+    h2 {
+        font-size: 18px;
+        font-style: bold;
+        text-align: center;
+    }
+`;
