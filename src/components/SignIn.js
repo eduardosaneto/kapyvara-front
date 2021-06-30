d8cf9e245350037827901fd5f22e7bae0f33a69d
Matheus Lira <matheustlira@gmail.com>
2021-06-30 17:11:53 -0300

    Static layout implemented

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,52 @@
-export default function SignIn(){
-    return (
-        <></>
-    );
+import { RegLoginContainer } from "../Structures/RegLoginContainer";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+import Logo from "../images/Logo.png";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabled, setDisabled] = useState(false);
+
+  const history = useHistory();
+
+  function Connect(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <>
+      <RegLoginContainer>
+        <img src={Logo} alt="Logo" />
+        <p className="title">Kapyvara</p>
+        <form onSubmit={(e) => Connect(e)}>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            placeholder="E-mail"
+            disabled={disabled}
+          ></input>
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="Senha"
+            disabled={disabled}
+          ></input>
+          <button onClick={Connect} disabled={disabled}>
+            {disabled ? (
+              <Loader type="ThreeDots" color="#FFF" height={40} width={45} />
+            ) : (
+              "Entrar"
+            )}
+          </button>
+        </form>
+        <Link to="/sign-up">
+          <p className="register">Primeira vez? Cadastre-se!</p>
+        </Link>
+      </RegLoginContainer>
+    </>
+  );
 }
