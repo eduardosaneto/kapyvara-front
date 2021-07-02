eb76a3a509b1ba5bd692f827d87cbfe313037c3f
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-07-02 17:08:11 -0300

    ajustes pequenos

--- src/components/Item.js
+++ src/components/Item.js
@@ -33,7 +33,6 @@ export default function Item({ image, name, price }){
         e.stopPropagation();
 
         if (operator === "-") {
-            chosenOption();
             count <= 1 ? unselectItem() : setCount(count - 1);
         } else {
             setCount(count + 1);
@@ -86,10 +85,10 @@ export default function Item({ image, name, price }){
                         </span>
                         <h2>R${price},00</h2>
                     </div>
-                    <AddBox selected={isSelected}>
+                    <AddBox selected={isSelected} onClick={chosenOption}>
                         {!isSelected ? 
                         <>
-                            <div><Plus onClick={chosenOption} /></div>
+                            <div><Plus /></div>
                             <h3>Add to Wallet</h3>
                         </>
                         :
